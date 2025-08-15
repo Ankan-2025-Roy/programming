39595a45d2e7d943eb3beefb1de2c951b0047dca
Ankan Roy <au2025ankan@gmail.com>
2025-08-15 19:07:18 +0530

    Add files via upload

---
+++ Assignment_3_D_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    printf("Enter Value of a:\n");
+    scanf("%d",&a);
+    printf("Enter Value of b:\n");
+    scanf("%d",&b);
+
+    sum = a + b;
+    printf("Sum of %d and %d is: %d\n", a, b, sum);
+
+}
