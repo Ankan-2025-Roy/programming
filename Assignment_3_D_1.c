39053112aedb5f75a06a82637eed6ce497c72aa5
Ankan Roy <au2025ankan@gmail.com>
2025-08-16 19:51:26 +0530

    Add files via upload

---
+++ Assignment_3_D_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sub;
+    printf("Enter Value of a:\n");
+    scanf("%d",&a);
+    printf("Enter Value of b:\n");
+    scanf("%d",&b);
+
+    sub = a - b;
+    printf("Subtracted b/w %d and %d is: %d\n", a, b, sub);
+
+}
