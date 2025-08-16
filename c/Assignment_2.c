dddac853bd9d22e51ce9d86598c1e13b6ec94a76
Ankan Roy <au2025ankan@gmail.com>
2025-08-16 20:01:46 +0530

    Delete c directory

--- c/Assignment_2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float a=4.3,b=5.6,x;
-    x=a+b;
-    
-    printf("%f",x);
-}
--- c/Assignment_5.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char var;
-    scanf("%c",&var);
-    printf("%d",var);
-}
