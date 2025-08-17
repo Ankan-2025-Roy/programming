6991e0fdcc4d507b6a1c13eb478346d0feb94d8d
Ankan Roy <au2025ankan@gmail.com>
2025-08-17 12:28:51 +0530

    Add files via upload

---
+++ Roy_Practice_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    float l,w,AOR;
+    
+    printf("Enter Value of Length :");
+
+    scanf("%f", &l);
+    printf("Enter Value of Width :");
+    scanf("%f", &w);
+    AOR = l * w;
+    printf("\nArea of Rectangle= %f", AOR);
+
+}
